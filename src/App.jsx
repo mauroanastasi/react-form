c4cf1e0180de7d828f514268fefc5fa8782338e8
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-12 19:18:24 +0200

    [UPD] mostro le stringe a pagina come li di un elenco mappando l'array, con onChange sul l'input ottengo il two-way data binding per inserire un testo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
 import { useState } from 'react'
 
-const initialConsole = [
+const initialConsoles = [
   "Dreamcast", "Game boy Color", "Playstaion 1", "PSP GO", "Xbox 360", "Playstation 5 Pro", "Nintendo 64", "NEC Pc Engine", "Nintendo Ds"];
 
 
 function App() {
-  const [] = useState()
-
+  const [newConsole, setNewConsole] = useState("");
 
   return (
     <>
-
+      <div className="container">
+        <h1>Console List</h1>
+        <ul className='list-group mb-3'>
+          {initialConsoles.map((initialConsole, index) => (
+            <li key={index} className="list-group-item">
+              {initialConsole}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <form>
+        <div className="input-group mb-3">
+          <input
+            value={newConsole}
+            onChange={(e) => {
+              setNewConsole(e.target.value);
+            }}
+            type="text"
+            className="form-control"
+            placeholder="Nuova Console"
+          />
+          <button className='btn btn-primary'>Add Console</button>
+        </div>
+      </form>
     </>
   )
 }
