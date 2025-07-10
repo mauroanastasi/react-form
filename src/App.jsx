de131945de6cd33699aff6f79968d15677b9bdfa
mauroanastasi <mauroanastasi1@gmail.com>
2025-07-10 18:07:15 +0200

    [UPD] add eliminate button

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import { useState } from 'react'
 
 const initialConsoles = [
-  "Dreamcast", "Game boy Color", "Playstaion 1", "PSP GO", "Xbox 360", "Playstation 5 Pro", "Nintendo 64", "NEC Pc Engine", "Nintendo Ds"];
+  "Dreamcast", "Game boy Color", "Playstaion 1", "PSP GO", "Xbox 360", "NEC Pc Engine", "Nintendo Ds"];
 
 
 function App() {
+  // variabile di stato che legge l'inserimento di una nuova console nell'input
   const [newConsole, setNewConsole] = useState("");
+
+  // variabile di stato inizializzato a initialConsole per aggiungere una console nuova senza modificare l'array di partenza
   const [consoles, setConsoles] = useState(initialConsoles);
 
 
-
+  // logica che attraverso lo spread operator applicata alla variabile di stato consoles spalma tutto il contenuto di initialConsoles per poi aggiungere il contenuto di newConsole e creare la nuova lista visualizzata a video
   const addConsole = (e) => {
     e.preventDefault()
     setConsoles([...consoles, newConsole])
+    // reimposta a stringa vuota l'input
     setNewConsole("")
+  }
 
+  // logica per eliminare una console dalla lista, come parametro viene passato l'index per capire quale elemento nell'array
+  const handleRemove = (index) => {
+    // prendo tutto consoles e attraverso il metodo splice cancello 1 elemento all'index corrente
+    const update = [...consoles];
+    update.splice(index, 1)
+    setConsoles(update)
   }
 
   return (
@@ -23,9 +34,12 @@ function App() {
         <h1>Console List</h1>
         <ul className='list-group mb-3'>
           {consoles.map((console, index) => (
-            <li key={index} className="list-group-item">
-              {console}
-            </li>
+            <>
+              <li key={index} className="list-group-item">
+                {console}
+                <button className='m-1' onClick={() => handleRemove(index)}>Elimina</button>
+              </li>
+            </>
           ))}
         </ul>
       </div>
